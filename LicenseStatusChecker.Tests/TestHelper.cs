6650403015c2d44e490cd5bd8c64ed6713182d2b
SirJUST <theoperaking@yahoo.com>
2019-10-15 20:11:41 -0400

    Add tests, modify gitignore

---
+++ LicenseStatusChecker.Tests/TestHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LicenseStatusChecker;
+
+namespace LicenseStatusChecker.Tests
+{
+    public static class TestHelper
+    {
+        public static List<Tradesman> GetMockTradesmen()
+        {
+            var tradesmen = new List<Tradesman>() {
+                new Tradesman{ Name= "JONES, A", ExpirationDate = DateTime.Now.AddDays(30).ToString(), HoursCompleted = 0, LicenseType = "PL" }
+            };
+            return tradesmen;
+        }
+
+        public static Tradesman GetMockTradesman()
+        {
+            return new Tradesman { Name = "JONES, A", ExpirationDate = DateTime.Now.AddDays(30).ToString(), HoursCompleted = 0, LicenseType = "PL" };
+        }
+    }
+}
---
+++ LicenseStatusChecker.Tests/TradesmanTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LicenseStatusChecker.Tests
+{
+    [TestClass]
+    public class TradesmanTests
+    {
+        [TestMethod]
+        public void GetHoursNeeded_ShouldReturnCorrectHours()
+        {
+            // Arrange
+            var tradesman = TestHelper.GetMockTradesman();
+            tradesman.Trade = "Plumber";
+            var expected = 16;
+
+            // Act
+            var actual = tradesman.GetHoursNeeded(tradesman.Trade);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
